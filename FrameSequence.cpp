93a182e3ef9a426037a96fb9fad04f9c7529ecc2
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-03-12 01:00:24 +0200

    Added implementation for makeFrame()

--- FrameSequence.cpp
+++ FrameSequence.cpp
@@ -2,14 +2,14 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <cmath>
 
 
-#define FRAMES MVHASA001::FrameSequence
 
-FRAMES::FrameSequence() : width(0), height(0), id(nullptr), source(nullptr)
+MVHASA001::FrameSequence::FrameSequence() : width(0), height(0), id(nullptr), source(nullptr)
 {}
 
-void FRAMES::readImage(std::string filename)
+void MVHASA001::FrameSequence::readImage(std::string filename)
 {
 	std::ifstream in(filename, std::ios::binary);
 
@@ -52,4 +52,34 @@ void FRAMES::readImage(std::string filename)
 	this->source = buffer;
 	this->height = height;
 	this->width = width;
+}
+
+void MVHASA001::FrameSequence::makeFrames(int windowHeight, int windowWidth, int x1, int y1, int x2, int y2)
+{
+	// Using y = mx + c to get line for tragectory
+	float m = (y2 - y1)/(x2-x1);
+	int c = y1;
+	int y = 0;
+
+	for (int x = x1; x < x2; x++)
+	{
+		y = m*x + c;
+		// floor the answer to get an integer
+		y = std::floor(y);
+
+		unsigned char ** frame;
+		frame = new unsigned char * [windowHeight];
+		for(int i = y; i < y+windowHeight; i++)
+		{
+
+			for(int j = x; j < x+windowWidth; j++)
+			{
+				unsigned char pixel = this->source[i * this->width + j];
+				frame[i][j] = pixel;
+			}
+		}
+
+		this->imageSequence.push_back(frame);
+	}
+
 }
