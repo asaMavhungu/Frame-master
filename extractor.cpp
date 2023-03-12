87d4000782c55a5a0951bc0568a8abcfc2c288c1
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-03-13 01:25:06 +0200

    Command line arguments now work as expected

--- extractor.cpp
+++ extractor.cpp
@@ -7,44 +7,80 @@ using namespace MVHASA001;
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello\n";
 	FrameSequence f;
 	std::string imageName = argv[1];
+
+
+
 	f.readImage(imageName);
 
-	int x1, y1, x2, y2;
-	int width, height;
-	std::string operation, name;
+	std::string x1;
+	std::string y1;
+	std::string x2;
+	std::string y2;
+	std::string width;
+	std::string height;
+	std::string operation;
+	std::string name;
+	
 
 	for (int i = 2; i < argc; ++i){
 
 		if ( strcmp( argv[i], "-t") == 0)
 		{
-			x1 = int(argv[i+1]);
-			i++;
-			y1 = int(argv[i+2]);
-			i++;
-			x2 = int(argv[i+3]);
-			i++;
-			y2 = int(argv[i+4]);
-			i++;
+			x1 = argv[i+1];
+			y1 = argv[i+2];
+			x2 = argv[i+3];
+			y2 = argv[i+4];
+			i++;i++;i++;i++;
 		}
 		else if ( strcmp (argv[i], "-s") == 0)
 		{
-			width = int(argv[i+1]);
-			i++;
-			height = int(argv[i+2]);
-			i++;
+			width = argv[i+1];
+			height = argv[i+2];
+			i++;i++;
 		}
 		else if ( strcmp (argv[i], "-w") == 0)
 		{
 			operation = argv[i+1];
-			i++;
 			name = argv[i+2];
-			i++;
+			i++;i++;
 		}
 	}
-	f.makeFrames(height, width, x1, y1, x2, y2);
+
+	int heightP, widthP, x1P, y1P, x2P, y2P;
+	heightP = std::stoi(height);
+	widthP = std::stoi(width);
+	x1P = std::stoi(x1);
+	y1P = std::stoi(y1);
+	x2P = std::stoi(x2);
+	y2P = std::stoi(y2);
+
+	f.makeFrames(heightP, widthP, x1P, y1P, x2P, y2P);
+
+	int frame = 0;
+	int i =0;
+	int j =0;
+
+
+	unsigned char* x = f.getSource();
+	unsigned char* y = f.getImageSequence()[0][0];
+
+	for (int i = 0; i < 100; i++)
+	{
+		unsigned char one = x[i];
+		unsigned char two = y[i];
+
+		bool ey = one == two;
+
+		//std::cout << ey << std::endl;
+		
+	}
+	
+	//std::cout << f.getSource()[i] ;
+	//std::cout << "\n" << f.getImageSequence()[frame][i][j] << std::endl;
+
+	f.writeFrames("asa.pgm", 0, 100, 100);
 
 	return 0;
 }
